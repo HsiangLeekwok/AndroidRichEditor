4e26844bbbb0d19e06a5809bd056a16d92ec3167
HsiangLeekwok <xiang.l.g@gmail.com>
2018-06-20 10:46:05 +0800

    img加载失败后自动尝试重新加载（10s延迟）

--- richeditor/src/main/assets/img_error_control.js
+++ richeditor/src/main/assets/img_error_control.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Copyright (C) 2017 Hsiang Leekwok
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,20 +13,36 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var imgs = ["lehehe.jpg", "potu.jpg", "loading_img_1.gif", "loading_img_2.gif", "loading_img_3.gif", "loading_img_4.gif", "loading_img_5.gif"];
+
+// var imgs = ["lehehe.jpg", "potu.jpg", "loading_img_1.gif", "loading_img_2.gif", "loading_img_3.gif", "loading_img_4.gif", "loading_img_5.gif"];
+
+var cache_attr = "cached-src";
+
 $(document).ready(function () {
+
     $("img").bind("error", function () {
+
         var obj = $(this);
-        $(obj).attr("cache-src", $(obj).attr("src"));
-        var index = Math.ceil(Math.random() * 100) % imgs.length;
-        $(obj).attr("src", imgs[index]);
+
+        // 缓存真正的 src 属性
+        $(obj).attr(cache_attr, $(obj).attr("src"));
+
+        // 显示加载失败
+        // var index = Math.ceil(Math.random() * 100) % imgs.length;
+        // $(obj).attr("src", imgs[index]);
+        $(obj).attr("src", "potu.jpg");
+
+        // 10s 尝试再次显示一下真正的 src 里的图片内容
         setTimeout(function () {
-            var src = $(obj).attr("cache-src");
+            var src = $(obj).attr(cache_attr);
             $(obj).attr("src", src);
         }, 10000);
-    }).bind("load", function(){
-        if($(this).attr("src").indexOf("http") == 0) {
-            $(this).removeAttr("cache-src");
+
+    }).bind("load", function () {
+        // 去掉多余的没用属性
+        if ($(this).attr("src").indexOf("http") == 0) {
+            $(this).removeAttr(cache_attr);
         }
     });
+
 });
